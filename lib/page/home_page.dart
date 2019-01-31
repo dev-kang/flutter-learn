3b3fdd6aa264acf4b06da8e3656144b26f56c25f
DevKang <cn.devkang@gmail.com>
2019-01-31 15:21:30 +0800

    修改页面打开动画效果

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -10,76 +10,77 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("开发tabBar"),
-          backgroundColor: Colors.red,
-        ),
-        // 头部标题的tabBar
-        // 侧边栏实现
-        drawer: new Drawer(
-          child: new ListView(
-            children: <Widget>[
-              new UserAccountsDrawerHeader(
-                decoration: new BoxDecoration(color: Colors.blue),
-                accountName: new Text('devKang'),
-                accountEmail: new Text("cn.devkang@gmail.com"),
-                currentAccountPicture: new GestureDetector(
+      appBar: new AppBar(
+        title: new Text("开发tabBar"),
+        backgroundColor: Colors.red,
+      ),
+      // 头部标题的tabBar
+      // 侧边栏实现
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              decoration: new BoxDecoration(color: Colors.blue),
+              accountName: new Text('devKang'),
+              accountEmail: new Text("cn.devkang@gmail.com"),
+              currentAccountPicture: new GestureDetector(
+                onTap: () => print('You click me!'),
+                child: new CircleAvatar(
+                  backgroundImage: new NetworkImage(
+                    "https://www.gravatar.com/avatar/default.jpg") // 设置头像,
+                ),
+              ),
+              otherAccountsPictures: <Widget>[
+                new GestureDetector(
                   onTap: () => print('You click me!'),
                   child: new CircleAvatar(
-                      backgroundImage: new NetworkImage(
-                          "https://www.gravatar.com/avatar/default.jpg") // 设置头像,
-                      ),
+                    backgroundImage: new NetworkImage(
+                      "https://www.gravatar.com/avatar/default.jpg") // 设置头像,
+                  ),
                 ),
-                otherAccountsPictures: <Widget>[
-                  new GestureDetector(
-                    onTap: () => print('You click me!'),
-                    child: new CircleAvatar(
-                        backgroundImage: new NetworkImage(
-                            "https://www.gravatar.com/avatar/default.jpg") // 设置头像,
-                        ),
+                new GestureDetector(
+                  onTap: () => print('You click me!'),
+                  child: new CircleAvatar(
+                    backgroundImage: new NetworkImage(
+                      "https://www.gravatar.com/avatar/default.jpg") // 设置头像,
                   ),
-                  new GestureDetector(
-                    onTap: () => print('You click me!'),
-                    child: new CircleAvatar(
-                        backgroundImage: new NetworkImage(
-                            "https://www.gravatar.com/avatar/default.jpg") // 设置头像,
-                        ),
+                ),
+                new GestureDetector(
+                  onTap: () => print('You click me!'),
+                  child: new CircleAvatar(
+                    backgroundImage: new NetworkImage(
+                      "https://www.gravatar.com/avatar/default.jpg") // 设置头像,
                   ),
-                  new GestureDetector(
-                    onTap: () => print('You click me!'),
-                    child: new CircleAvatar(
-                        backgroundImage: new NetworkImage(
-                            "https://www.gravatar.com/avatar/default.jpg") // 设置头像,
-                        ),
-                  ),
-                ],
-              ),
-              new ListTile(
-                title: new Text("open the first page"),
-                trailing: new Icon(Icons.arrow_right),
-                onTap: () {
-                  Navigator.of(context).pop(); //点击后收起侧边栏
-                  Navigator.of(context).push(new MaterialPageRoute(
-                      builder: (BuildContext context) => new OtherPage('First Page'))); //进入OtherPage页面，传入参数First Page
-                },
-              ),
-              new ListTile(
-                title: new Text("open the second page"),
-                trailing: new Icon(Icons.arrow_right),
-                onTap: () {
+                ),
+              ],
+            ),
+            new ListTile(
+              title: new Text("open the first page"),
+              trailing: new Icon(Icons.arrow_right),
+              onTap: () {
+                Navigator.of(context).pop(); //点击后收起侧边栏
+                Navigator.of(context).push(new MaterialPageRoute(
+                  builder: (BuildContext context) =>
+                  new OtherPage('First Page'))); //进入OtherPage页面，传入参数First Page
+              },
+            ),
+            new ListTile(
+              title: new Text("open the second page"),
+              trailing: new Icon(Icons.arrow_right),
+              onTap: () {
 //                  Navigator.of(context).pop();
-                  Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new OtherPage('Second Page')));
-                },
-              ),
-            ],
-          ),
+                Navigator.push(context, new OtherPageRoute("second page"));
+              },
+            ),
+          ],
         ),
-        // 头部标题栏 左侧图标，
-        body: new Center(
-          child: new Text(
-            "HomePage: hello world！",
-            style: new TextStyle(fontSize: 20),
-          ),
-        ));
+      ),
+      // 头部标题栏 左侧图标，
+      body: new Center(
+        child: new Text(
+          "HomePage: hello world！",
+          style: new TextStyle(fontSize: 20),
+        ),
+      ));
   }
 }
--- lib/page/other_page.dart
+++ lib/page/other_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class OtherPage extends StatelessWidget {
   
   final String pageText;
 
   OtherPage(this.pageText);    //构造函数，获取参数
-  
-  
+
+
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
@@ -16,5 +17,22 @@ class OtherPage extends StatelessWidget {
         child: new Text(pageText),
       ),
     );
+  }
+}
+
+
+class OtherPageRoute extends CupertinoPageRoute {
+  
+  final String pageText;
+  
+  OtherPageRoute(this.pageText)
+    : super(builder: (BuildContext context) => new OtherPage(pageText));
+  
+  
+  // OPTIONAL IF YOU WISH TO HAVE SOME EXTRA ANIMATION WHILE ROUTING
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+    Animation<double> secondaryAnimation) {
+      return FadeTransition(opacity: animation, child: new OtherPage(pageText),);
   }
 }
