822c5d1c51bd9c93d43e797341a4231e35f8c48b
DevKang <cn.devkang@gmail.com>
2019-01-31 14:37:10 +0800

    增加左侧栏内容

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,22 +2,81 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   @override
-
   _HomePageState createState() => new _HomePageState();
-
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
-  Widget build(BuildContext text) {
+  Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("开发tabBar"), backgroundColor: Colors.red,), // 头部标题的tabBar
-      drawer: new Drawer(), // 头部标题栏 左侧图标，
-      body: new Center(
-        child: new Text("HomePage: hello world！", style: new TextStyle(fontSize: 20),),
-      )
+        appBar: new AppBar(
+          title: new Text("开发tabBar"),
+          backgroundColor: Colors.red,
+        ),
+        // 头部标题的tabBar
+        // 侧边栏实现
+        drawer: new Drawer(
+          child: new ListView(
+            children: <Widget>[
+              new UserAccountsDrawerHeader(
+                decoration: new BoxDecoration(color: Colors.blue),
+                accountName: new Text('devKang'),
+                accountEmail: new Text("cn.devkang@gmail.com"),
+                currentAccountPicture: new GestureDetector(
+                  onTap: () => print('You click me!'),
+                  child: new CircleAvatar(
+                    backgroundImage: new NetworkImage("https://www.gravatar.com/avatar/default.jpg") // 设置头像,
+                  ),
+                ),
+                otherAccountsPictures: <Widget>[
+                  new GestureDetector(
+                    onTap: () => print('You click me!'),
+                    child: new CircleAvatar(
+                      backgroundImage: new NetworkImage("https://www.gravatar.com/avatar/default.jpg") // 设置头像,
+                    ),
+                  ),
 
-    );
+                  new GestureDetector(
+                    onTap: () => print('You click me!'),
+                    child: new CircleAvatar(
+                      backgroundImage: new NetworkImage("https://www.gravatar.com/avatar/default.jpg") // 设置头像,
+                    ),
+                  ),
+
+                  new GestureDetector(
+                    onTap: () => print('You click me!'),
+                    child: new CircleAvatar(
+                      backgroundImage: new NetworkImage("https://www.gravatar.com/avatar/default.jpg") // 设置头像,
+                    ),
+                  ),
+                ],
+              ),
+              
+              new ListTile(
+                title: new Text("the first function"),
+                trailing: new Icon(Icons.arrow_right),
+                onTap: () {
+                  Navigator.of(context).pop;
+                },
+              ),
+              
+              new ListTile(
+                title: new Text("the second function"),
+                trailing: new Icon(Icons.arrow_right),
+                onTap: () {
+                  Navigator.of(context).pop;
+                },
+              ),
+              
+            ],
+          ),
+        ),
+        // 头部标题栏 左侧图标，
+        body: new Center(
+          child: new Text(
+            "HomePage: hello world！",
+            style: new TextStyle(fontSize: 20),
+          ),
+        ));
   }
 }
